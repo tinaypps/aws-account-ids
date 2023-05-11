4f4afe19f49d49a42e3400936cd36e185f1527e9
Pavan Yara <55478779+pavan-yara@users.noreply.github.com>
2023-05-11 18:45:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,67 +1,54 @@
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
-        const tableBody = document.getElementById('table-body');
-        const paginationDiv = document.getElementById('pagination');
-        const searchInput = document.getElementById('search');
-
-        let pageSize = 10;
+        let pageSize = 5;
         let currentPage = 1;
-
-        function paginate(array, pageSize, pageNumber) {
-            return array.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
-        }
+        let totalPage = Math.ceil(data.length / pageSize);
 
         // Function to display data in the table
-        function displayData(data) {
+        function displayData() {
+            let start = (currentPage - 1) * pageSize;
+            let end = start + pageSize;
+
             // Clear the table
-            tableBody.innerHTML = '';
+            document.getElementById('table-body').innerHTML = '';
 
-            const paginatedData = paginate(data, pageSize, currentPage);
-
-            // Display a slice of the data in the table
-            paginatedData.forEach(item => {
-                const row = document.createElement('tr');
-                row.innerHTML = `
-                    <td>${item.owner}</td>
-                    <td>${item.description}</td>
-                    <td>${item.id}</td>
-                    <td><a href="${item.source}" target="_blank">Link</a></td>
+            data.slice(start, end).forEach(item => {
+                document.getElementById('table-body').innerHTML += `
+                    <tr>
+                        <td>${item.owner}</td>
+                        <td>${item.description}</td>
+                        <td>${item.id}</td>
+                        <td><a href="${item.source}" target="_blank">Link</a></td>
+                    </tr>
                 `;
-                tableBody.appendChild(row);
             });
 
-            // Display the pagination
-            displayPagination(data.length, pageSize);
-        }
-
-        // Function to display pagination
-        function displayPagination(totalItems, itemsPerPage) {
-            // Calculate the number of pages
-            const numPages = Math.ceil(totalItems / itemsPerPage);
-
-            // Clear the pagination
-            paginationDiv.innerHTML = '';
-
-            // Add a button for each page
-            for (let i = 1; i <= numPages; i++) {
-                const btn = document.createElement('button');
-                btn.classList.add('btn', 'btn-light', 'mx-1');
-                btn.innerText = i;
-                btn.addEventListener('click', function() {
-                    currentPage = i;
-                    displayData(data);
-                });
-                paginationDiv.appendChild(btn);
+            document.getElementById('pagination').innerHTML = '';
+            for(let i = 1; i <= totalPage; i++) {
+                document.getElementById('pagination').innerHTML += `<button class="page">${i}</button>`;
             }
         }
 
-        // Display all data by default
-        displayData(data);
+        displayData();
 
-        // Search function
-        searchInput.addEventListener('keyup', (e) => {
-            const searchString = e.target.value.toLowerCase();
+        document.getElementById('pagination').addEventListener('click', function(e) {
+            if(e.target.className === 'page') {
+                currentPage = Number(e.target.textContent);
+                displayData();
+            }
+        });
+
+        document.getElementById('search').addEventListener('keyup', function(e) {
+            let searchString = e.target.value.toLowerCase();
 
             // Filter data
-            const filteredData = data
+            data = data.filter(item => item.owner.toLowerCase().includes(searchString) || 
+                item.description.toLowerCase().includes(searchString) || 
+                item.id.includes(searchString));
+
+            totalPage = Math.ceil(data.length / pageSize);
+            currentPage = 1;
+            displayData();
+        });
+    });
