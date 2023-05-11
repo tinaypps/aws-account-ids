5c956f62c3cbaa4dfa156e06cab7599b8381b848
Pavan Yara <55478779+pavan-yara@users.noreply.github.com>
2023-05-11 18:36:35 +0530

    update html code for pagination

--- app.js
+++ app.js
@@ -2,21 +2,58 @@ fetch('data.json')
     .then(response => response.json())
     .then(data => {
         const tableBody = document.getElementById('table-body');
+        const paginationDiv = document.getElementById('pagination');
         const searchInput = document.getElementById('search');
+
+        let pageSize = 10;
+        let currentPage = 1;
+
+        function paginate(array, pageSize, pageNumber) {
+            return array.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
+        }
 
         // Function to display data in the table
         function displayData(data) {
+            // Clear the table
             tableBody.innerHTML = '';
-            data.forEach(item => {
-                tableBody.innerHTML += `
-                    <tr>
-                        <td>${item.owner}</td>
-                        <td>${item.description}</td>
-                        <td><a href="${item.source}" target="_blank">Link</a></td>
-                        <td>${item.id}</td>
-                    </tr>
+
+            const paginatedData = paginate(data, pageSize, currentPage);
+
+            // Display a slice of the data in the table
+            paginatedData.forEach(item => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${item.owner}</td>
+                    <td>${item.description}</td>
+                    <td>${item.id}</td>
+                    <td><a href="${item.source}" target="_blank">Link</a></td>
                 `;
+                tableBody.appendChild(row);
             });
+
+            // Display the pagination
+            displayPagination(data.length, pageSize);
+        }
+
+        // Function to display pagination
+        function displayPagination(totalItems, itemsPerPage) {
+            // Calculate the number of pages
+            const numPages = Math.ceil(totalItems / itemsPerPage);
+
+            // Clear the pagination
+            paginationDiv.innerHTML = '';
+
+            // Add a button for each page
+            for (let i = 1; i <= numPages; i++) {
+                const btn = document.createElement('button');
+                btn.classList.add('btn', 'btn-light', 'mx-1');
+                btn.innerText = i;
+                btn.addEventListener('click', function() {
+                    currentPage = i;
+                    displayData(data);
+                });
+                paginationDiv.appendChild(btn);
+            }
         }
 
         // Display all data by default
@@ -27,16 +64,4 @@ fetch('data.json')
             const searchString = e.target.value.toLowerCase();
 
             // Filter data
-            const filteredData = data.filter(item => {
-                return (
-                    item.owner.toLowerCase().includes(searchString) ||
-                    item.description.toLowerCase().includes(searchString) ||
-                    item.id.includes(searchString)
-                );
-            });
-
-            // Display filtered data
-            displayData(filteredData);
-        });
-    })
-    .catch(error => console.error(error));
+            const filteredData = data
